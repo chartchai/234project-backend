7830a7b89e614d839df79dae6aafeb7f79fc4c6f
JunYu Zhou <feijifeibudao@gmail.com>
2018-05-08 16:50:17 +0700

    add AuthenticationServiceImplTest.java
    test receive the username, and password then return its role

---
+++ src/test/java/camt/se234/project/ProductServiceImplTests.java
@@ -0,0 +1,36 @@
+package camt.se234.project;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class ProductServiceImplTests {
+    @Before
+    public void setup(){
+
+    }
+
+    @Test
+    public void testGetAllProducts(){
+
+    }
+
+    @Test
+    public void testGetAvailableProducts(){
+
+    }
+
+    @Test
+    public void testGetUnavailableProductSize(){
+
+    }
+
+    @Test
+    public void testGetSaleOrders(){
+
+    }
+
+    @Test
+    public void testGetAverageSaleOrderPrice(){
+
+    }
+}
