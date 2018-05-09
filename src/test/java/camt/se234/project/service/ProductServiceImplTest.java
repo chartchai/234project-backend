f125d74a904b53c0dbfae535c00c36e80616e991
Unknown <chamnol_yin@cmu.ac.th>
2018-05-09 21:26:21 +0700

    implement getAvialableProduct test method

--- src/test/java/camt/se234/project/service/ProductServiceImplTest.java
+++ src/test/java/camt/se234/project/service/ProductServiceImplTest.java
@@ -1,5 +1,6 @@
 package camt.se234.project.service;
 
+import camt.se234.project.Exceptions.NoDataException;
 import camt.se234.project.dao.ProductDao;
 import camt.se234.project.entity.Product;
 import org.junit.Before;
@@ -9,8 +10,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.hamcrest.Matchers.hasItem;
+import static org.hamcrest.core.Is.is;
 import static org.hamcrest.core.IsNull.nullValue;
 import static org.junit.Assert.assertThat;
+import static org.mockito.Matchers.isNull;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -36,6 +39,22 @@ public class ProductServiceImplTest {
     @Test(expected = AssertionError.class)
     public void testGetProductsWithException(){
         assertThat(productService.getAllProducts(), nullValue());
+    }
+
+    @Test
+    public void testGetAvailableProducts(){
+        List<Product> mockProducts = new ArrayList<>();
+        mockProducts.add(new Product("D001","Milk","Drink","image/",2500));
+        when(productDao.getProducts()).thenReturn(mockProducts);
+        assertThat(productService.getAvailableProducts(), hasItem(new Product("D001","Milk","Drink","image/",2500)));
+    }
+
+    @Test (expected = NoDataException.class)
+    public void testGetAvailableProductsNoData(){
+        List<Product> mockProducts = new ArrayList<>();
+        mockProducts.add(new Product("D001","Milk","Drink","image/",-2500));
+        when(productDao.getProducts()).thenReturn(mockProducts);
+        assertThat(productService.getAvailableProducts(), is(isNull()));
     }
 
 }
