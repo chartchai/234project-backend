968a6c8be5370542f81e2fd155aed968d86e84b5
Unknown <chamnol_yin@cmu.ac.th>
2018-05-09 21:08:52 +0700

    generate Product class's equal and hashCode

--- src/main/java/camt/se234/project/entity/Product.java
+++ src/main/java/camt/se234/project/entity/Product.java
@@ -9,6 +9,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import java.util.Objects;
 
 @Entity
 @Data
@@ -31,6 +32,25 @@ public class Product {
         this.description = description;
         this.imageLocation = imageLocation;
         this.price = price;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Product)) return false;
+        Product product = (Product) o;
+        return Double.compare(product.price, price) == 0 &&
+                Objects.equals(id, product.id) &&
+                Objects.equals(productId, product.productId) &&
+                Objects.equals(name, product.name) &&
+                Objects.equals(description, product.description) &&
+                Objects.equals(imageLocation, product.imageLocation);
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(id, productId, name, description, imageLocation, price);
     }
 
 }
